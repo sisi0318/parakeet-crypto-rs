94dd90abd5d7f59434877cd1cb06b692f4d50830
Jixun Wu <jixun.moe@gmail.com>
2024-02-01 20:36:14 +0000

    refactor: unify qmc1 names

--- src/crypto/tencent/mod.rs
+++ src/crypto/tencent/mod.rs
@@ -1,7 +1,7 @@
 use map::map_l;
 
 mod map;
-mod qmc;
+mod qmc1;
 mod qmc2_map;
 mod qmc2_rc4;
 
@@ -9,7 +9,7 @@ mod ekey;
 mod rc4;
 mod tail;
 
-pub use qmc::decrypt_qmc1;
+pub use qmc1::decrypt_qmc1;
 pub use qmc2_map::Version2Map;
 pub use qmc2_rc4::Version2RC4;
 pub use tail::parse_tail;
---
+++ src/crypto/tencent/qmc1.rs
@@ -0,0 +1,9 @@
+pub fn decrypt_qmc1(offset: usize, buffer: &mut [u8]) {
+    let qmc1_map = include_bytes!("./qmc1.bin");
+
+    let mut i = offset;
+    for item in buffer {
+        *item ^= super::map_l(qmc1_map, i);
+        i += 1;
+    }
+}
